b3ea054b7b8332b18987adbbd4e43486b3edeb7d
Mateus Moreira <xpsykogfx@gmail.com>
2018-09-17 21:46:14 +0100

    1.13.1 Initial commit.

---
+++ src/main/java/me/badbones69/blockparticles/api/ParticleType.java
@@ -0,0 +1,18 @@
+package me.badbones69.blockparticles.api;
+
+public enum ParticleType {
+
+    PARTICLE("Particle"), FOUNTAIN("Fountain");
+
+    String Name;
+
+    private ParticleType(String name) {
+        this.Name = name;
+    }
+
+    public static ParticleType[] getTypes() {
+        ParticleType[] p = ParticleType.values();
+        return p;
+    }
+
+}
---
+++ src/main/java/org/inventivetalent/particle/ParticleException.java
@@ -0,0 +1,22 @@
+package org.inventivetalent.particle;
+
+public class ParticleException extends RuntimeException {
+    public ParticleException() {
+    }
+
+    public ParticleException(String message) {
+        super(message);
+    }
+
+    public ParticleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ParticleException(Throwable cause) {
+        super(cause);
+    }
+
+    public ParticleException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
