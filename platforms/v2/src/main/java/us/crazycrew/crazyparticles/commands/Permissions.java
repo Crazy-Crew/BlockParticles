929327aa717d7c4a8d7e9af5916331d12c819b72
Ryder Belserion <no-reply@ryderbelserion.com>
2023-03-09 22:33:19 -0500

    Blank slate the permissions enum

--- platforms/v2/src/main/java/us/crazycrew/crazyparticles/commands/Permissions.java
+++ platforms/v2/src/main/java/us/crazycrew/crazyparticles/commands/Permissions.java
@@ -10,92 +10,9 @@ import java.util.HashMap;
 
 public enum Permissions {
 
-    CRATES_COMMAND_PLAYER_HELP("crazycrates.command.player.help", "Shows all player commands for CrazyCrates.", new HashMap<>() {{
-        put(prefix + ".command.player.key", true);
-    }}, PermissionDefault.TRUE),
+    PARTICLES_COMMAND_PLAYER_HELP("command.player.help", "Shows all player commands for CrazyParticles.", new HashMap<>(), PermissionDefault.TRUE),
 
-    CRATES_COMMAND_PLAYER_MENU("command.player.menu", "Opens the primary crate menu.", null, PermissionDefault.TRUE),
-    CRATES_COMMAND_PLAYER_KEY("command.player.key", "Check the number of keys you have.", null, PermissionDefault.OP),
-    CRATES_COMMAND_PLAYER_KEY_OTHERS("command.player.key.others", "Check the number of keys a player has.", new HashMap<>() {{
-        put(prefix + ".command.player.key", true);
-    }}, PermissionDefault.OP),
-
-    CRATES_COMMAND_PLAYER_ROOT("command.players.*", "Gives access to all player commands.", new HashMap<>() {{
-        put(prefix + ".command.player.key.others", true);
-        put(prefix + ".command.player.help", true);
-        put(prefix + ".command.player.menu", true);
-    }}, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_ROOT("command.admin.*", "Gives access to all admin commands.", new HashMap<>() {{
-        put(prefix + ".command.admin.schematic.*", true);
-        put(prefix + ".command.admin.help", true);
-        put(prefix + ".command.admin.convert", true);
-        put(prefix + ".command.admin.debug", true);
-        put(prefix + ".command.admin.setmenu", true);
-        put(prefix + ".command.admin.giveall", true);
-        put(prefix + ".command.admin.forceopen", true);
-        put(prefix + ".command.admin.open.others", true);
-        put(prefix + ".command.admin.list", true);
-        put(prefix + ".command.admin.preview", true);
-        put(prefix + ".command.admin.additem", true);
-        put(prefix + ".command.admin.access", true);
-    }}, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_ADDITEM("command.admin.additem", "Add items in-game to a prize in a crate.", null, PermissionDefault.OP),
-    CRATES_COMMAND_ADMIN_ACCESS("command.admin.access", "Opens the Admin Keys GUI.", null, PermissionDefault.OP),
-    CRATES_COMMAND_ADMIN_PREVIEW("command.admin.preview", "Opens the preview of a crate for a player.", null, PermissionDefault.OP),
-    CRATES_COMMAND_ADMIN_LIST("command.admin.list", "Displays a list of all crates.", new HashMap<>() {{
-        put(prefix + ".command.admin.teleport", true);
-    }}, PermissionDefault.OP),
-    CRATES_COMMAND_ADMIN_OPEN("command.admin.open", "Tries to open a crate for yourself if you have a key.", null, PermissionDefault.OP),
-    CRATES_COMMAND_ADMIN_MASS_OPEN("command.admin.mass-open", "Mass opens crates. Defaults to 10 but can be changed in the crate config files.", null, PermissionDefault.OP),
-    CRATES_COMMAND_ADMIN_OPEN_OTHERS("command.admin.open.others", "Tries to open a crate for a player if they have a key.", new HashMap<>() {{
-        put(prefix + ".command.admin.open", true);
-    }}, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_TAKEKEY("command.admin.takekey", "Allows you to take keys from a player.", new HashMap<>() {{
-        put(prefix + ".command.player.transfer", true);
-    }}, PermissionDefault.OP),
-
-    CRATES_COMMAND_PLAYER_TRANSFER("command.player.transfer", "Allows players to send virtual keys to another player.", null, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_FORCEOPEN("command.admin.forceopen", "Opens a crate for a player for free.", new HashMap<>() {{
-        put(prefix + ".command.admin.open", true);
-        put(prefix + ".command.admin.open.others", true);
-    }}, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_TELEPORT("command.admin.teleport", "Teleport to a crate.", null, PermissionDefault.OP),
-    CRATES_COMMAND_ADMIN_GIVEKEY("command.admin.givekey", "Give a key(s) to a player to use on a crate.", null, PermissionDefault.OP),
-    CRATES_COMMAND_ADMIN_GIVERANDOMKEY("command.admin.giverandomkey", "Gives a random key out of all the crates available.", null, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_GIVEALL("command.admin.giveall", "Gives all online players keys to use on a crate.", new HashMap<>() {{
-        put(prefix + ".command.admin.givekey", true);
-        put(prefix + ".command.admin.giverandomkey", true);
-        put(prefix + ".command.admin.takekey", true);
-        put(prefix + ".command.player.transfer", true);
-    }}, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_SET("command.admin.set", "Set a block you are looking at as the specified crate.", null, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_SETMENU("command.admin.setmenu", "", new HashMap<>() {{
-        put(prefix + ".command.admin.set", true);
-    }}, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_RELOAD("command.admin.reload", "Reloads the configuration and data files.", null, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_DEBUG("command.admin.debug", "Allows you to spit out a bunch of information.", null, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_CONVERT("command.admin.convert", "Tries to convert supported plugin's crate files into crazy crate's crate files.", null, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_SCHEMATIC_ROOT("command.admin.schematic.*", "Gives access to all commands relating to schematics.", new HashMap<>() {{
-        put(prefix + ".command.admin.schematic.save", true);
-        put(prefix + ".command.admin.schematic.set", true);
-    }}, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_SCHEMATIC_SAVE("command.admin.schematic.save", "Save the new schematic file to the schematics folder. 1.13+ only", null, PermissionDefault.OP),
-    CRATES_COMMAND_ADMIN_SCHEMATIC_SET("command.admin.schematic.set", "Set position #1 or #2 for when making a new schematic for quadcrates. 1.13+ only", null, PermissionDefault.OP),
-
-    CRATES_COMMAND_ADMIN_HELP("command.admin.help", "", new HashMap<>() {{
+    PARTICLES_COMMAND_ADMIN_HELP("command.admin.help", "Shows all admin commands for CrazyParticles", new HashMap<>() {{
         put(prefix + ".command.admin.reload", true);
     }}, PermissionDefault.OP);
 
