2eb7f9b8ed45a2904e887a2be41afef1085ce94e
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-24 02:00:41 -0400

    add location util

---
+++ paper/src/main/java/com/badbones69/blockparticles/util/LocationUtil.java
@@ -0,0 +1,30 @@
+package com.badbones69.blockparticles.util;
+
+import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
+
+public class LocationUtil {
+
+    /**
+     * Converts a location object to a string for storage
+     *
+     * @param location the {@link Location} to convert
+     * @param getName true or false
+     * @return the location string
+     */
+    public static @NotNull String location(@NotNull final Location location, boolean getName) {
+        String name = getName ? location.getWorld().getName() : String.valueOf(location.getWorld().getUID());
+
+        return name + "," + location.getBlockX() + "," + location.getBlockY() + "," + location.getBlockZ();
+    }
+
+    /**
+     * Converts a {@link Location} object with the {@link org.bukkit.World} {@link java.util.UUID}
+     *
+     * @param location the {@link Location}
+     * @return the location string
+     */
+    public static @NotNull String location(@NotNull final Location location) {
+        return location(location, false);
+    }
+}
