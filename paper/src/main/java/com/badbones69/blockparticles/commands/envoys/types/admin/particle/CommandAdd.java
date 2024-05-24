ac4f8910b687ad37b172033a2d5d06eeba48b2b8
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-24 02:01:26 -0400

    Add particle suggestion to add command

--- paper/src/main/java/com/badbones69/blockparticles/commands/envoys/types/admin/particle/CommandAdd.java
+++ paper/src/main/java/com/badbones69/blockparticles/commands/envoys/types/admin/particle/CommandAdd.java
@@ -4,6 +4,7 @@ import com.badbones69.blockparticles.Methods;
 import com.badbones69.blockparticles.commands.envoys.BaseCommand;
 import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.annotations.Command;
+import dev.triumphteam.cmd.core.annotations.Suggestion;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionDefault;
 
@@ -11,7 +12,7 @@ public class CommandAdd extends BaseCommand {
 
     @Command("add")
     @Permission(value = "blockparticles.add", def = PermissionDefault.OP)
-    public void add(Player player, String id) {
+    public void add(Player player, String id, @Suggestion("particles") String particle) {
         Methods.addLoc(player, id);
     }
 }
