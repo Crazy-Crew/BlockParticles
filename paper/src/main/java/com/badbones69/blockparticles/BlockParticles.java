eeacf36138bcae713de3562fb5dc8af3b4aacea4
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-25 01:20:08 -0400

    clean up main class

--- paper/src/main/java/com/badbones69/blockparticles/BlockParticles.java
+++ paper/src/main/java/com/badbones69/blockparticles/BlockParticles.java
@@ -1,17 +1,13 @@
 package com.badbones69.blockparticles;
 
 import com.badbones69.blockparticles.api.ParticleHandler;
-import com.badbones69.blockparticles.api.ParticleManager;
-import com.badbones69.blockparticles.controllers.ParticleControl;
+import com.badbones69.blockparticles.commands.CommandManager;
 import com.ryderbelserion.vital.paper.VitalPaper;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlockParticles extends JavaPlugin {
 
     private ParticleHandler particleHandler;
-
-    private ParticleManager particleManager;
-    private ParticleControl particleControl;
     
     @Override
     public void onEnable() {
@@ -21,43 +17,17 @@ public class BlockParticles extends JavaPlugin {
         server.apply();
 
         this.particleHandler = new ParticleHandler();
-        this.particleHandler.load(true);
+        this.particleHandler.load();
 
-        //this.particleManager = new ParticleManager();
-        //this.particleManager.load();
-
-        //this.particleControl = new Particles();
-
-        //getServer().getPluginManager().registerEvents(new MainMenu(), this);
-        //getServer().getPluginManager().registerEvents(new Methods(), this);
-        //getServer().getPluginManager().registerEvents(new Fountains(), this);
-        //getServer().getPluginManager().registerEvents(new ItemPickUp(), this);
-
-        // Load the commands.
-        //CommandManager.load();
-
-        /*new BukkitRunnable() {
-            @Override
-            public void run() {
-                Methods.startParticles();
-            }
-        }.runTaskLater(this, 200);*/
+        CommandManager.load();
     }
 
     @Override
     public void onDisable() {
-        //Methods.kill();
+        super.onDisable();
     }
 
     public final ParticleHandler getParticleHandler() {
         return this.particleHandler;
-    }
-
-    public final ParticleManager getParticleManager() {
-        return this.particleManager;
-    }
-
-    public final ParticleControl getParticleControl() {
-        return this.particleControl;
     }
 }
