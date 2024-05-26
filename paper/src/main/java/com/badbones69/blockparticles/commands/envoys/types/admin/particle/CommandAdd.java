51425fbe33417e15b2b7062dd5f9716e3e146df6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-26 02:28:23 -0400

    update add particle command

--- paper/src/main/java/com/badbones69/blockparticles/commands/envoys/types/admin/particle/CommandAdd.java
+++ paper/src/main/java/com/badbones69/blockparticles/commands/envoys/types/admin/particle/CommandAdd.java
@@ -1,16 +1,19 @@
 package com.badbones69.blockparticles.commands.envoys.types.admin.particle;
 
+import com.badbones69.blockparticles.api.builders.particles.types.SpiralParticle;
 import com.badbones69.blockparticles.api.enums.Messages;
 import com.badbones69.blockparticles.api.enums.particles.ParticleKey;
 import com.badbones69.blockparticles.api.builders.particles.types.GenericParticle;
-import com.badbones69.blockparticles.api.builders.particles.types.SpiralParticle;
 import com.badbones69.blockparticles.api.interfaces.IParticleBuilder;
 import com.badbones69.blockparticles.commands.envoys.BaseCommand;
-import com.badbones69.blockparticles.utils.ParticleUtil;
 import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.annotations.Command;
-import dev.triumphteam.cmd.core.annotations.Optional;
+import dev.triumphteam.cmd.core.annotations.CommandFlags;
+import dev.triumphteam.cmd.core.annotations.Flag;
+import dev.triumphteam.cmd.core.annotations.NamedArguments;
 import dev.triumphteam.cmd.core.annotations.Suggestion;
+import dev.triumphteam.cmd.core.argument.keyed.Arguments;
+import dev.triumphteam.cmd.core.argument.keyed.Flags;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
@@ -23,16 +26,14 @@ public class CommandAdd extends BaseCommand {
 
     @Command("add")
     @Permission(value = "blockparticles.add", def = PermissionDefault.OP)
-    public void add(Player player, String id, @Suggestion("types") String type, Particle particle, @Suggestion("numbers") int count, @Optional @Suggestion("numbers") int size) {
-        //todo() tell them it can't be empty.
-        if (type.isEmpty()) return;
-
-        final ParticleKey particleKey = ParticleUtil.getParticleByName(type);
-
-        //todo() tell them it can't be null.
-        if (particleKey == null) return;
-
-        Block block = player.getTargetBlockExact(player.getViewDistance());
+    @CommandFlags({
+       @Flag(longFlag = "inner", argument = int.class, suggestion = "integers"),
+       @Flag(longFlag = "outer", argument = int.class, suggestion = "integers"),
+       @Flag(longFlag = "height", argument = double.class, suggestion = "doubles")
+    })
+    @NamedArguments("bp_add")
+    public void add(Player player, @Suggestion("uuids") String id, Arguments arguments, Flags flags) {
+        final Block block = player.getTargetBlockExact(player.getViewDistance());
 
         if (block == null || block.getType() == Material.AIR) {
             Messages.not_looking_at_block.sendMessage(player);
@@ -40,14 +41,23 @@ public class CommandAdd extends BaseCommand {
             return;
         }
 
-        Location location = block.getLocation();
+        final ParticleKey particleKey = arguments.getArgument("key", ParticleKey.class).orElse(ParticleKey.GENERIC);
+        final Particle particle = arguments.getArgument("particle", Particle.class).orElse(Particle.FLAME);
+        final int amount = arguments.getArgument("amount", Integer.class).orElse(1);
+        final Location location = block.getLocation();
 
         IParticleBuilder builder = null;
 
         switch (particleKey) {
-            case GENERIC -> builder = new GenericParticle(id, count, particleKey, particle, location.add(0.5, 1.0, 0.5));
+            case SPIRAL, DOUBLE_SPIRAL -> {
+                boolean isPresent = flags.getFlagValue("inner").isPresent();
 
-            case SPIRAL, DOUBLE_SPIRAL -> builder = new SpiralParticle(id, count, size, particleKey, particle, location.add(0.5, 1.1, 0.5));
+                if (isPresent) {
+                    builder = new SpiralParticle(id, amount, 0, 0, 0, particleKey, particle, location);
+                }
+            }
+
+            case GENERIC -> builder = new GenericParticle(id, amount, particleKey, particle, location);
         }
 
         if (builder != null) {
