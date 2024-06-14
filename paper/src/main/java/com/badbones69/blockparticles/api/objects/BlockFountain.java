b43bce5aca03ee3505c628c6c7fed971fe79c2f8
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-14 16:40:05 -0400

    stash file changes

---
+++ paper/src/main/java/com/badbones69/blockparticles/api/objects/BlockFountain.java
@@ -0,0 +1,38 @@
+package com.badbones69.blockparticles.api.objects;
+
+import com.ryderbelserion.vital.paper.builders.items.ItemBuilder;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BlockFountain {
+
+    private final List<ItemStack> builtHeads = new ArrayList<>();
+    private final List<String> fountainHeads;
+    private final String fountainName;
+
+    public BlockFountain(String fountainName, List<String> fountainHeads) {
+        this.fountainName = fountainName;
+        this.fountainHeads = fountainHeads;
+
+        for (String head : this.fountainHeads) {
+            ItemStack item = new ItemBuilder().setPlayer(head).getStack();
+
+            this.builtHeads.add(item);
+        }
+    }
+
+    public @NotNull final List<ItemStack> getBuiltHeads() {
+        return Collections.unmodifiableList(this.builtHeads);
+    }
+
+    public @NotNull final List<String> getFountainHeads() {
+        return Collections.unmodifiableList(this.fountainHeads);
+    }
+
+    public @NotNull final String getFountainName() {
+        return this.fountainName;
+    }
+}
