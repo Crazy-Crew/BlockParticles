fbd2b5ddb7f4deed02cf9d2853468a0a87f42b12
Ryder Belserion <no-reply@ryderbelserion.com>
2024-09-02 22:54:17 -0400

    update buildscript

--- buildSrc/src/main/kotlin/paper-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/paper-plugin.gradle.kts
@@ -1,13 +1,9 @@
-import com.ryderbelserion.feather.enums.Repository
-
 plugins {
     id("java-plugin")
 }
 
 repositories {
-    maven("https://repo.triumphteam.dev/snapshots")
+    maven("https://repo.papermc.io/repository/maven-public")
 
     maven("https://maven.enginehub.org/repo")
-
-    maven(Repository.Paper.url)
 }
