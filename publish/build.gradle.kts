687a33db5c5c7ed55fbeb8e5e30143689a83a11d
Ryder Belserion <no-reply@ryderbelserion.com>
2024-10-22 22:10:47 -0400

    [ci-skip] remove dependencies from publish task

--- publish/build.gradle.kts
+++ publish/build.gradle.kts
@@ -32,10 +32,6 @@ tasks {
 
         autoAddDependsOn.set(false)
         detectLoaders.set(false)
-
-        dependencies {
-            optional.version("fancyholograms", "2.3.2")
-        }
     }
 
     hangarPublish {
@@ -55,28 +51,6 @@ tasks {
                     jar.set(rootProject.projectDir.resolve("jars/$pluginName-${rootProject.version}.jar"))
 
                     platformVersions.set(listOf(mcVersion))
-
-                    dependencies {
-                        hangar("PlaceholderAPI") {
-                            required = false
-                        }
-
-                        hangar("FancyHolograms") {
-                            required = false
-                        }
-
-                        url("Oraxen", "https://www.spigotmc.org/resources/%E2%98%84%EF%B8%8F-oraxen-custom-items-blocks-emotes-furniture-resourcepack-and-gui-1-18-1-20-4.72448/") {
-                            required = false
-                        }
-
-                        url("CMI", "https://www.spigotmc.org/resources/cmi-298-commands-insane-kits-portals-essentials-economy-mysql-sqlite-much-more.3742/") {
-                            required = false
-                        }
-
-                        url("DecentHolograms", "https://www.spigotmc.org/resources/decentholograms-1-8-1-20-4-papi-support-no-dependencies.96927/") {
-                            required = false
-                        }
-                    }
                 }
             }
         }
